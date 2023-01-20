c3c8fb880b1d9d1de434627f0864e97f4cbb22ac
Spencer White <email@spencerwhite.xyz>
2023-01-19 20:26:52 -0500

    Added more `std` types
    Added Option<B>
    Added String
    Added char
    Added tuples

--- instrs-core/src/lib.rs
+++ instrs-core/src/lib.rs
@@ -1,13 +1,15 @@
 #![feature(array_try_from_fn)]
 #![feature(split_array)]
+#![feature(trace_macros)]
 
 use num_traits::Unsigned;
-use std::fmt::{Formatter, Error as FError};
 
 #[derive(Debug)]
 pub enum Error {
     ExpectedBytes(u32),
     ExpectedRange(std::ops::RangeInclusive<u32>),
+    InvalidUtf8(std::string::FromUtf8Error),
+    InvalidChar,
 }
 
 pub trait Serialize: Sized {
@@ -43,6 +45,67 @@ impl_byte_serialize!(u8,u16,u32,u64,u128,usize,
                      // Using signed numbers in bytecode is stupid but I still support it because I am a nice person
                      i8,i16,i32,i64,i128,isize,
                      f32,f64);
+
+impl Serialize for char {
+    fn from_bytes(b: &mut &[u8]) -> Result<Self, Error> {
+        match char::from_u32(u32::from_bytes(b)?) {
+            Some(c) => Ok(c),
+            None => Err(Error::InvalidChar),
+        }
+    }
+
+    fn into_bytes(&self, b: &mut Vec<u8>) {
+        (*self as u32).into_bytes(b)
+    }
+}
+
+impl Serialize for bool {
+    fn from_bytes(b: &mut &[u8]) -> Result<Self, Error> {
+        let Some((byte, rem)) = b.split_first() else {return Err(Error::ExpectedBytes(1))};
+        *b = rem;
+
+        match byte {
+            0 => Ok(false),
+            1 => Ok(true),
+            _ => Err(Error::ExpectedRange(0..=1)),
+        } 
+    }
+
+    fn into_bytes(&self, b: &mut Vec<u8>) {
+        (*self as u8).into_bytes(b)
+    }
+}
+
+macro_rules! impl_byte_serialize_tuple {
+    ( $($i:ident $n:tt),* ) => {
+        #[allow(unused_variables)]
+        impl<$($i : Serialize),*> Serialize for ( $($i,)* ) {
+            fn from_bytes(b: &mut &[u8]) -> Result<Self, Error> {
+                Ok((
+                    $($i::from_bytes(b)?,)*
+                ))
+            }
+
+            fn into_bytes(&self, b: &mut Vec<u8> ) {
+                $(self.$n.into_bytes(b);)*
+            }
+        }
+    }
+}
+
+impl_byte_serialize_tuple!();
+impl_byte_serialize_tuple!(A 0);
+impl_byte_serialize_tuple!(A 0, B 1);
+impl_byte_serialize_tuple!(A 0, B 1, C 2);
+impl_byte_serialize_tuple!(A 0, B 1, C 2, D 3);
+impl_byte_serialize_tuple!(A 0, B 1, C 2, D 3, E 4);
+impl_byte_serialize_tuple!(A 0, B 1, C 2, D 3, E 4, F 5);
+impl_byte_serialize_tuple!(A 0, B 1, C 2, D 3, E 4, F 5, G 6);
+impl_byte_serialize_tuple!(A 0, B 1, C 2, D 3, E 4, F 5, G 6, H 7);
+impl_byte_serialize_tuple!(A 0, B 1, C 2, D 3, E 4, F 5, G 6, H 7, J 8);
+impl_byte_serialize_tuple!(A 0, B 1, C 2, D 3, E 4, F 5, G 6, H 7, J 8, K 9);
+impl_byte_serialize_tuple!(A 0, B 1, C 2, D 3, E 4, F 5, G 6, H 7, J 8, K 9, L 10);
+
 
 impl<B: Serialize, const N: usize> Serialize for [B; N] {
     fn from_bytes(b: &mut &[u8]) -> Result<Self, Error> {
@@ -82,6 +145,19 @@ impl<B: Serialize> Serialize for Vec<B> {
     }
 }
 
+impl<B: Serialize> Serialize for Box<[B]> {
+    fn from_bytes(b: &mut &[u8]) -> Result<Self, Error> {
+        Ok(<Vec<B>>::from_bytes(b)?.into_boxed_slice())
+    }
+
+    fn into_bytes(&self, b: &mut Vec<u8>) {
+        self.iter()
+            .for_each(|inner| {
+                inner.into_bytes(b);
+            });
+    }
+}
+
 impl<B: Serialize> Serialize for Box<B> {
     fn from_bytes(b: &mut &[u8]) -> Result<Self, Error> {
         Ok(Box::new(B::from_bytes(b)?))
@@ -92,4 +168,46 @@ impl<B: Serialize> Serialize for Box<B> {
     }
 }
 
-//TODO: Add Option<B>
+impl<B: Serialize> Serialize for Option<B> {
+    fn from_bytes(b: &mut &[u8]) -> Result<Self, Error> {
+        let is_some = bool::from_bytes(b)?;
+
+        match is_some {
+            false => Ok(None),
+            true => Ok(Some(B::from_bytes(b)?)),
+        }
+    }
+
+    fn into_bytes(&self, b: &mut Vec<u8>) {
+        self.is_some().into_bytes(b);
+
+        if let Some(inner) = self {
+            inner.into_bytes(b);
+        }
+    }
+}
+
+impl Serialize for String {
+    fn from_bytes(b: &mut &[u8]) -> Result<Self, Error> {
+        match std::string::String::from_utf8(<Vec<u8>>::from_bytes(b)?) {
+            Ok(s) => Ok(s),
+            Err(e) => Err(Error::InvalidUtf8(e)),
+        }
+    }
+
+    fn into_bytes(&self, b: &mut Vec<u8>) {
+        b.push(self.len() as u8);
+        b.extend_from_slice(self.as_bytes())
+    }
+}
+
+impl Serialize for &str {
+    fn from_bytes(_b: &mut &[u8]) -> Result<Self, Error> {
+        panic!("Can't convert bytes to &'str. Try converting to an owned `String` instead")
+    }
+
+    fn into_bytes(&self, b: &mut Vec<u8>) {
+        b.push(self.len() as u8);
+        b.extend_from_slice(self.as_bytes())
+    }
+}
--- tests/tests/bytes.rs
+++ tests/tests/bytes.rs
@@ -2,14 +2,56 @@ use tests::Instruction;
 
 use instrs::*;
 
+fn test_into_bytes<T: Serialize>(t: T, expected: &[u8]) {
+    let mut v = Vec::new();
+    T::into_bytes(&t, &mut v);
+    assert_eq!(v, expected);
+}
+
+fn test_from_bytes<T: Serialize + std::fmt::Debug + PartialEq>(mut t: &[u8], expected: T) {
+    let value = T::from_bytes(&mut t).expect(&format!("You mucked up the test for {:?}", expected));
+    assert_eq!(value, expected);
+}
+
+#[test]
+fn number() {
+    test_into_bytes(1u32, &[1,0,0,0]); //Big Endian!
+    test_into_bytes(-1i32, &[255,255,255,255]);
+
+    test_from_bytes(&[255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255], u128::MAX);
+}
+
+#[test]
+fn bool() {
+    test_into_bytes(true, &[1]);
+    test_from_bytes(&[0], false);
+}
+
+#[test]
+fn char() {
+    test_into_bytes('A', &[65,0,0,0]);
+    test_from_bytes(&[5, 39, 0, 0], '✅');
+}
+
+#[test]
+fn tuple() {
+    test_into_bytes((), &[]);
+    test_from_bytes(&[1, 2, 3, 4, 5], (1u8, 2u8, 3u8, 4u8, 5u8));
+}
+
+#[test]
+fn array() {
+    test_into_bytes(['A', 'B', 'C'], &[65,0,0,0,66,0,0,0,67,0,0,0]);
+    test_from_bytes(&[65,0,0,0,66,0,0,0,67,0,0,0], ['A', 'B', 'C']);
+}
+#[test]
+fn string() {
+    test_into_bytes(String::from("ABC"), &[3,65,66,67]);
+    test_from_bytes(&[3,65,66,67], String::from("ABC"));
+}
+
 #[test]
 fn into_bytes() {
-    fn test_into_bytes<T: Serialize>(t: T, expected: &[u8]) {
-        let mut v = Vec::new();
-        T::into_bytes(&t, &mut v);
-        assert_eq!(v, expected);
-    }
-
     test_into_bytes(Instruction::Add {a: 1, b: 2, addr: 3}, &[0, 1, 2, 3]);
     test_into_bytes(Instruction::Jump (u32::MAX), &[1, u8::MAX, u8::MAX, u8::MAX, u8::MAX]);
     test_into_bytes(Instruction::Halt, &[2]);
@@ -17,11 +59,6 @@ fn into_bytes() {
 
 #[test]
 fn from_bytes() {
-    fn test_from_bytes<T: Serialize + std::fmt::Debug + PartialEq>(mut t: &[u8], expected: T) {
-        let value = T::from_bytes(&mut t).expect(&format!("You mucked up the test for {:?}", expected));
-        assert_eq!(value, expected);
-    }
-
     test_from_bytes(&[32], 32u8);
     test_from_bytes(&[255,255,255,255], u32::MAX);
     test_from_bytes(&[2,1,2,], vec![1u8, 2]);
